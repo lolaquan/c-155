ad0ea6491bf7d90e415653d4cc345dcf6bb6606b
lolaquan <71743830+lolaquan@users.noreply.github.com>
2022-07-24 09:47:49 -0700

    Add files via upload

---
+++ Collider.js
@@ -0,0 +1,40 @@
+AFRAME.registerComponent("flying-birds", {
+    init: function () {
+        for (var i = 1; i <= 20; i++) {
+            var id = 'hurdle${i}';
+
+            var posX = Math.floor(Math.random() * 3000 + -1000);
+            var posY = Math.floor(Math.random() * 2 + -1);
+            var posZ = Math.floor(Math.random() * 3000 + -1000);
+
+            var position = { x: posX, y: posY, z: posZ };
+
+            this.flyingBirds(id, position);
+        }
+    },
+    flyingBirds: (id, position) => {
+        var terrainEl = document.querySelector("#terrain")
+
+        var birdEl = document.createElement("a-entity");
+
+        birdEl.setAttribute("id", id);
+
+        birdEl.setAttribute("position", position);
+        birdEl.setAttribute("scale", { x: 500, y: 500, z: 500});
+
+        birdEl.setAttribute("gltf-model", "./assets/models/flying_bird/scene.gltf")
+
+        birdEl.setAttribute("animation-mixer", {});
+
+        birdEl.setAttribute("static-body", {
+            shape: "sphere",
+            sphereRadius: 5,
+        });
+
+        birdEl.setAttribute("game-play", {
+            elementId: '#${id}',
+        });
+
+        terrainEl.appendChild(birdEl);
+    },
+});
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# c-155
+# PRO-C153-Student-Activity
